bbe1bd0597d044268cde29dd61737241d328e90b
Ajit Ratan Singh <ajit555@gmail.com>
2018-06-17 01:22:21 +0530

    Blank solution files added

---
+++ src/B12.js
@@ -0,0 +1,12 @@
+// Goal
+// SQL: 
+
+const R = require('ramda');
+const cities = require('./data/cities.json');
+
+// Solution function To be implemented
+let fnB = () => (cities)
+
+let resultB = fnB(cities)
+
+console.log(resultB);
